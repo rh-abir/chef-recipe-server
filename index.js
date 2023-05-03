4406e0c654ba50200f8a1b1f4fbaa5031e65749b
rh-abir <rhabir.co@gmail.com>
2023-05-04 00:05:20 +0600

    make single id data link

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const app = express();
 
-const allData = require('./data/chefsData.json')
+const allData = require("./data/chefsData.json");
 
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send('hello from my server')
-})
+app.get("/", (req, res) => {
+  res.send("hello from my server");
+});
 
-app.get('/alldata', (req, res) => {
-    res.send(allData)
-})
+app.get("/alldata", (req, res) => {
+  res.send(allData);
+});
 
+app.get("/alldata/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id)
+  const selectedChef = allData.find(n => n.id === id);
+
+  res.send(selectedChef)
+  
+});
 
 app.listen(port, () => {
-    console.log(`my server is running ${port}`)
-})
+  console.log(`my server is running ${port}`);
+});
